744ab287eb5ad851c9d9e3f9f9d7885da42f204f
maxahn <ahnmaximus@gmail.com>
2020-05-28 17:26:43 -0700

    task component skeleton and dummy data for task

---
+++ src/Task/dummydata.js
@@ -0,0 +1,52 @@
+let uniqueID = 0;
+const user = {
+  id: 0,
+  firstName: 'Max',
+  lastName: 'Ahn',
+  email: 'ahnmaxwell@gmail.com',
+  password: '12345', // remember to encrypt
+  tasks: tasks, 
+  friends: [], // id of friends in the future, many to many relationship? 
+  // bits owned -> can be calculated from tasks?
+}
+
+const tasks = [
+  {
+    id: uniqueID++,
+    title: "Create dummy data for Task component",
+    description: "For 436I",
+    givenClaps: [1, 2, 3],  // id of users who has given claps 
+    bitTransactions: [
+      {
+        donorID: 1,
+        bitAmount: 10,
+        timestamp: new Date()
+      },
+      {
+        donorID: 2,
+        bitAmount: 5,
+        timestamp: new Date()
+      },
+      {
+        donorID: 3,
+        bitAmount: 10,
+        timestamp: new Date()
+      }
+    ],
+    timestamp: new Date()
+  },
+  {
+    id: uniqueID++,
+    title: "Create task component",
+    description: "For 436I",
+    givenClaps: [3],  // id of users who has given claps 
+    bitTransactions: [
+      {
+        donorID: 2,
+        bitAmount: 1,
+        timestamp: new Date()
+      }
+    ],
+    timestamp: new Date()
+  }
+]
