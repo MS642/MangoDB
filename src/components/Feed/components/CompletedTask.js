1a68d6b6e8bb99a59a85af4eac0ffb04a6bd9f1b
aamirs <aamirs@students.cs.ubc.ca>
2020-06-05 15:26:40 -0700

    updated styling

--- src/components/Feed/components/CompletedTask.js
+++ src/components/Feed/components/CompletedTask.js
@@ -6,10 +6,10 @@ class CompletedTask extends React.Component {
 
     state = {
         compTasks: [
-            {user: "Mangostine Coconutbottom", avatarURL: "https://i.ytimg.com/vi/dJmF1UCAkwc/maxresdefault.jpg",
-                task: "Making a tinfoil hat to protect myself from 5G.", timestamp: "December 31, 1999 23:59:59"},
+            {user: "Mangosteen Coconutbottom", avatarURL: "https://i.ytimg.com/vi/dJmF1UCAkwc/maxresdefault.jpg",
+                task: "Making a tinfoil hat to protect myself from 5G.", timestamp: "December 31, 1999 23:59:59", id:0},
             {user: "Tiger Dude", avatarURL: "https://i.cbc.ca/1.4818848.1536687959!/cpImage/httpImage/oklahoma-governor-candidate-charged.jpg",
-            task: "Buying pet food for my pet tiger.", timestamp: "March 5, 2020 12:00:00"}
+            task: "Buying pet food for my pet tiger.", timestamp: "March 5, 2020 12:00:00", id:1}
         ]
     };
 
@@ -18,10 +18,10 @@ class CompletedTask extends React.Component {
             <div className={"container CompletedTask"}>
                 <div className={"row"}>
                     <div className={"col-1 AvatarCol"}>
-                        <img src="https://i.ytimg.com/vi/dJmF1UCAkwc/maxresdefault.jpg" width={"50px"} height={"50px"} className={"userAvatar"} alt={""} />
+                        <img src="https://i.imgur.com/wAy6yQt.jpg" width={"60px"} height={"60px"} className={"userAvatar"} alt={""} />
                     </div>
                     <div className={"col-9 d-flex justify-content-start text-start"}>
-                        <span><strong>Mangosteen Coconutbottom </strong> started the task "Making a tinfoil hat to protect myself from 5G."!</span>
+                        <span><strong>Mangosteen Coconutbottom </strong> started the task "Making a tinfoil hat to protect myself from 5G cellular towers"!</span>
                     </div>
                     <div className={"col-2 TimestampCol d-flex justify-content-end text-end"}>
                         20 Years Ago
