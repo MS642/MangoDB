546ae3167d246a94a4ef696e584ded42b115ef4d
aamirs <aamirs@students.cs.ubc.ca>
2020-06-05 11:48:25 -0700

    updated styling

--- src/components/Feed/components/SocialUnit.js
+++ src/components/Feed/components/SocialUnit.js
@@ -5,16 +5,16 @@ class SocialUnit extends React.Component {
         return (
             <div className={"container SocialUnit"}>
                 <div className={"row"}>
-                    <div className={"col-2 socialClap"}>
+                    <div className={"col-1 socialClap"}>
                         <button className={"clapButton"}><img className={"clapButtonImg"} src={"https://i.imgur.com/tToSF7j.png"}
-                                     width={"30px"} height={"30px"} alt={""}/> 24</button>
+                                     width={"30px"} height={"30px"} alt={""}/>24</button>
                     </div>
                     <div className={"col-2 socialMango"}>
-
+                        <span><img className={"mangoSocialImg"} src={"https://i.imgur.com/U79Rb8V.png"}
+                             width={"40px"} height={"20px"} alt={""}/>
+                             <strong>999</strong><button className={"addMangoButton"}><strong>+</strong></button></span>
                     </div>
-                    <div className={"col-1 socialGiveMango"}>
 
-                    </div>
                 </div>
 
             </div>
