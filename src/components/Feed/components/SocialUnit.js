7ce2d8a97af124b666720ed35876088f49d44d29
aamirs <aamirs@students.cs.ubc.ca>
2020-06-05 12:04:16 -0700

    updated styling

--- src/components/Feed/components/SocialUnit.js
+++ src/components/Feed/components/SocialUnit.js
@@ -3,13 +3,13 @@ import * as React from "react";
 class SocialUnit extends React.Component {
     render() {
         return (
-            <div className={"container SocialUnit"}>
+            <div className={"container align-items-center SocialUnit"}>
                 <div className={"row"}>
                     <div className={"col-1 socialClap"}>
                         <button className={"clapButton"}><img className={"clapButtonImg"} src={"https://i.imgur.com/tToSF7j.png"}
-                                     width={"30px"} height={"30px"} alt={""}/>24</button>
+                                     width={"25px"} height={"25px"} alt={""}/>24</button>
                     </div>
-                    <div className={"col-2 socialMango"}>
+                    <div className={"col-11 justify-content-start socialMango"}>
                         <span><img className={"mangoSocialImg"} src={"https://i.imgur.com/U79Rb8V.png"}
                              width={"40px"} height={"20px"} alt={""}/>
                              <strong>999</strong><button className={"addMangoButton"}><strong>+</strong></button></span>
