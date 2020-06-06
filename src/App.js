e81902fd0c2206bf2d740943db02d8db39642fd0
Ryan Oh <ohryan55@gmail.com>
2020-06-05 21:25:36 -0700

    Added the bootstrap version of the navbar, although it doesnt seem to add much so far

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import NavBarItem from "./components/NavBar/components/NavBarItem";
 import TaskPage from "./scenes/TaskPage";
 import ProfilePage from "./scenes/ProfilePage";
 import HomePage from "./scenes/HomePage";
+import {Nav, Navbar, NavItem, NavLink} from "react-bootstrap";
+import ErrorPage from "./scenes/ErrorPage";
+import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 
 const HOME_ICON_URL = "home_icon.svg";
 const TASK_ICON_URL = "task_icon.png";
@@ -17,22 +20,24 @@ class App extends React.Component {
     render() {
         return(
             <Router>
-                <div className="navbar">
-                    <Link to={'/'}>
-                        <NavBarItem name="Home" img={HOME_ICON_URL}/>
-                    </Link>
-
-                    <Link to={'/tasks'}>
-                        <NavBarItem name="Tasks" img={TASK_ICON_URL}/>
-                    </Link>
-
-                    <div id="right-align">
-                        <Link to={'/profile'}>
-                            <NavBarItem name="Profile" img={PROFILE_ICON_URL}/>
-                        </Link>
-                    </div>
-                </div>
-                <div className="centered-div">
+                <Navbar fluid collapseOnSelect>
+                    <Navbar.Brand as={Link} to={'/'}>
+                        <img className="navbar-image" src={HOME_ICON_URL} />
+                    </Navbar.Brand>
+                    <Navbar.Collapse>
+                        <Nav className="mr-auto">
+                            <NavLink as={Link} to="/tasks" >
+                                <NavBarItem img={TASK_ICON_URL} />
+                            </NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                    <NavLink as={Link} to="/profile" >
+                        <div className="ml-auto">
+                            <NavBarItem img={PROFILE_ICON_URL} />
+                        </div>
+                    </NavLink>
+                </Navbar>
+                <div>
                     <Switch>
                         <Route path="/tasks">
                             <TaskPage />
@@ -40,8 +45,11 @@ class App extends React.Component {
                         <Route path="/profile">
                             <ProfilePage />
                         </Route>
-                        <Route path="/">
+                        <Route exact path="/">
                             <HomePage />
+                        </Route>
+                        <Route>
+                            <ErrorPage />
                         </Route>
                     </Switch>
                 </div>
@@ -49,5 +57,38 @@ class App extends React.Component {
         )
     }
 }
+
+{/*<Router>*/}
+{/*    <Navbar>*/}
+{/*        <Navbar.Brand as={Link} to={'/'}>*/}
+{/*            <NavBarItem name="Home" img={HOME_ICON_URL}/>*/}
+{/*        </Navbar.Brand>*/}
+
+{/*        <Nav.Link href='/tasks'>*/}
+{/*            test*/}
+{/*        </Nav.Link>*/}
+{/*        <div id="right-align">*/}
+{/*            <Navbar.Brand as={Link} to={'/profile'}>*/}
+{/*                <NavBarItem name="Profile" img={PROFILE_ICON_URL}/>*/}
+{/*            </Navbar.Brand>*/}
+{/*        </div>*/}
+{/*    </Navbar>*/}
+{/*    <div className="centered-div">*/}
+{/*        <Switch>*/}
+{/*            <Route path="/tasks">*/}
+{/*                <TaskPage />*/}
+{/*            </Route>*/}
+{/*            <Route path="/profile">*/}
+{/*                <ProfilePage />*/}
+{/*            </Route>*/}
+{/*            <Route exact path="/">*/}
+{/*                <HomePage />*/}
+{/*            </Route>*/}
+{/*            <Route>*/}
+{/*                <ErrorPage />*/}
+{/*            </Route>*/}
+{/*        </Switch>*/}
+{/*    </div>*/}
+{/*</Router>*/}
 
 export default App;
