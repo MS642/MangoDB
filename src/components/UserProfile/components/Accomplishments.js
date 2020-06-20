8357d50d22c01478fd4c01a3db7de3447a1c2249
aamirs <aamirs@students.cs.ubc.ca>
2020-06-20 03:11:34 -0700

    user profile updates

--- src/components/UserProfile/components/Accomplishments.js
+++ src/components/UserProfile/components/Accomplishments.js
@@ -15,16 +15,16 @@ class Accomplishments extends React.Component {
     const {mangosRec, clapsRec, tasksComp} = this.props;
     return (
       <div style={{ border: "3px solid black", borderRadius: "6px" }}>
-        <Carousel interval={1500}>
+        <Carousel interval={3000}>
 
           <Carousel.Item>
             <div className={"bg-secondary"} style={{ width: "100%", height: "200px"}}>
               <div className={"row"}>
                 <div className={"col-4 d-flex justify-content-center align-items-center"}>
-                  <img width={"200px"} height={"200px"} src="potato_mango.png" alt=""/>
+                  <img className={"mangoImg"} width={"200px"} height={"200px"} src="potato_mango.png" alt=""/>
                 </div>
                 <div className={"col-8 d-flex justify-content-start align-items-center"}>
-                    <h3 className={"mangoAward"}>You have been awarded {mangosRec} mangos!</h3>
+                    <h2 className={"mangoAward"}>You were awarded {mangosRec} mangos for taking initiative!</h2>
                 </div>
               </div>
             </div>
@@ -34,10 +34,10 @@ class Accomplishments extends React.Component {
             <div className={"bg-secondary"} style={{ width: "100%", height: "200px"}}>
               <div className={"row"}>
                 <div className={"col-4 d-flex justify-content-center align-items-center"}>
-                  <img width={"200px"} height={"200px"} src="potato_mango.png" alt=""/>
+                  <img width={"200px"} height={"200px"} src="clap.png" alt=""/>
                 </div>
                 <div className={"col-8 d-flex justify-content-start align-items-center"}>
-                  <h3 className={"mangoAward"}>You have been awarded {mangosRec} mangos!</h3>
+                  <h2 className={"mangoAward"}>You were applauded with {clapsRec} claps for a job well done!</h2>
                 </div>
               </div>
             </div>
@@ -47,10 +47,10 @@ class Accomplishments extends React.Component {
             <div className={"bg-secondary"} style={{ width: "100%", height: "200px"}}>
               <div className={"row"}>
                 <div className={"col-4 d-flex justify-content-center align-items-center"}>
-                  <img width={"200px"} height={"200px"} src="potato_mango.png" alt=""/>
+                  <img className={"taskImg"} width={"200px"} height={"200px"} src="task_icon.png" alt=""/>
                 </div>
                 <div className={"col-8 d-flex justify-content-start align-items-center"}>
-                  <h3 className={"mangoAward"}>You have been awarded {mangosRec} mangos!</h3>
+                  <h2 className={"mangoAward"}>You completed {tasksComp} tasks! Wow!</h2>
                 </div>
               </div>
             </div>
