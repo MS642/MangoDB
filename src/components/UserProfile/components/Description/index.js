5a0843115940f8e92885ebaa62b59b0eb4b6c857
AamirL1011 <aamirs@students.cs.ubc.ca>
2020-07-05 12:08:58 -0700

    username editor

--- src/components/UserProfile/components/Description/index.js
+++ src/components/UserProfile/components/Description/index.js
@@ -1,4 +1,8 @@
 import * as React from "react";
+import "../../UserProfile.css";
+import CreateIcon from '@material-ui/icons/Create';
+import { IconButton } from '@material-ui/core';
+
 
 class UserDescription extends React.Component {
   render() {
@@ -6,9 +10,13 @@ class UserDescription extends React.Component {
     return (
       <div id={"nameBox"}>
         <div className="row">
-          <div className="col text-center">
-            <p id="nameField">{name}</p>
+          <div className="col-9 d-flex justify-content-center align-items-center text-center">
+            {name}
           </div>
+          <div className="col-2 d-flex justify-content-center text-center">
+            <IconButton><CreateIcon id={"nameEditIcon"}/></IconButton>
+          </div>
+          <div className={"col-1"}></div>
         </div>
       </div>
     );
