bf64e94d7e985e148cafaf15b6559ff79ccec8f6
AamirL1011 <aamirs@students.cs.ubc.ca>
2020-07-04 22:48:46 -0700

    avatar editor updates

--- src/components/UserProfile/components/Avatar/components/PhotoModal/index.js
+++ src/components/UserProfile/components/Avatar/components/PhotoModal/index.js
@@ -2,9 +2,30 @@ import * as React from "react";
 import "../../../../UserProfile.css";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Cropper from 'react-easy-crop';
+
 
 
 class PhotoModal extends React.Component {
+
+  state = {
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png',
+    crop: { x: 0, y: 0 },
+    zoom: 1,
+    aspect: 4 / 3,
+  };
+
+  onCropChange = crop => {
+    this.setState({ crop })
+  };
+
+  onCropComplete = (croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels)
+  };
+
+  onZoomChange = zoom => {
+    this.setState({ zoom })
+  };
 
   render() {
     return(
@@ -17,16 +38,23 @@ class PhotoModal extends React.Component {
         >
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-vcenter">
-              Modal heading
+              Photo Editor
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Centered Modal</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-              consectetur ac, vestibulum at eros.
-            </p>
+            <div className={"row"}>
+              <div className={"col"} style={{height:"400px"}}>
+                <Cropper
+                  image={this.state.image}
+                  crop={this.state.crop}
+                  zoom={this.state.zoom}
+                  aspect={this.state.aspect}
+                  onCropChange={this.onCropChange}
+                  onCropComplete={this.onCropComplete}
+                  onZoomChange={this.onZoomChange}
+                />
+              </div>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.props.onHide}>Close</Button>
