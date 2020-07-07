76c54343985a6e05e883788f40d753a1b0f59efe
AamirL1011 <aamirs@students.cs.ubc.ca>
2020-07-07 01:49:51 -0700

    updated profile

--- src/components/UserProfile/components/Description/index.js
+++ src/components/UserProfile/components/Description/index.js
@@ -5,6 +5,10 @@ import { IconButton } from '@material-ui/core';
 import { connect } from "react-redux";
 import {updateName} from "./actions";
 import SaveIcon from '@material-ui/icons/Save';
+import TextField from '@material-ui/core/TextField';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
 
 class UserDescription extends React.Component {
 
@@ -34,12 +38,24 @@ class UserDescription extends React.Component {
 
 
   render() {
+    const theme = createMuiTheme({
+      palette: {
+        primary: {
+          light: '#fcb540',
+          main: '#FCA311',
+          dark: '#b0720b',
+          contrastText: '#fff',
+        },
+      },
+    });
     const { currUserName } = this.props;
     return (
       <div id={"nameBox"}>
         <div className="row">
-          <div className="col-9 d-flex justify-content-center align-items-center">
-            <input id={"nameInput"} onChange={this.onNameChange} style={{backgroundColor: (this.state.nameEditActive)? "#343a40": "#4a535c"}} type="text" defaultValue={currUserName}  disabled={this.state.nameEditActive}/>
+          <div className="col-9 inputDiv d-flex justify-content-center align-items-center">
+            <ThemeProvider theme={theme}>
+            <TextField  multiline id={"nameInput"} onChange={this.onNameChange} style={{backgroundColor: (this.state.nameEditActive)? "#343a40": "#4a535c"}} type="text" defaultValue={currUserName}  disabled={this.state.nameEditActive}/>
+            </ThemeProvider>
           </div>
           <div className="col-2 d-flex justify-content-center text-center">
             <IconButton id={"nameEditBtn"} onClick={this.makeEditActive}>{(this.state.nameEditActive)? <CreateIcon id={"nameEditIcon"} /> : <SaveIcon id={"nameSaveIcon"} onClick={this.onNameSubmit}/>}</IconButton>
