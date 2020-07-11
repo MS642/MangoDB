91d59c49e201b48ef03131a4f6c6aac6f0dc9630
maxahn <ahnmaximus@gmail.com>
2020-07-10 19:02:41 -0700

    added ryans fix and redux tool

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import reducers from "./reducers";
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(thunk)
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
