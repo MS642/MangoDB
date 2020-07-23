57a64edd15cd40dbb9a6afe90aa45e71e403a8f2
AamirL1011 <aamirs@students.cs.ubc.ca>
2020-07-23 16:42:09 -0700

    Update index.js

--- src/components/Store/components/Shelf/index.js
+++ src/components/Store/components/Shelf/index.js
@@ -13,19 +13,37 @@ class Shelf extends React.Component {
               <i className="material-icons verified">verified</i>
             </div>
             <div>
-              <i className="material-icons verified">verified</i>
+              <i className="material-icons verified">pets</i>
             </div>
             <div>
-              <i className="material-icons verified">verified</i>
+              <i className="material-icons verified">android</i>
             </div>
             <div>
-              <i className="material-icons verified">verified</i>
+              <i className="material-icons verified">bug_report</i>
             </div>
             <div>
-              <i className="material-icons verified">verified</i>
+              <i className="material-icons verified">eco</i>
             </div>
             <div>
-              <i className="material-icons verified">verified</i>
+              <i className="material-icons verified">grade</i>
+            </div>
+            <div>
+              <i className="material-icons verified">sentiment_satisfied_alt</i>
+            </div>
+            <div>
+              <i className="material-icons verified">biotech</i>
+            </div>
+            <div>
+              <i className="material-icons verified">weekend</i>
+            </div>
+            <div>
+              <i className="material-icons verified">videogame_asset</i>
+            </div>
+            <div>
+              <i className="material-icons verified">headset</i>
+            </div>
+            <div>
+              <i className="material-icons verified">palette</i>
             </div>
           </div>
         </div>
