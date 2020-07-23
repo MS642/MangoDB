14ee212f2b00b2e0616796e803c63183bd121bf3
maxahn <ahnmaximus@gmail.com>
2020-07-22 17:07:43 -0700

    removed weird padding and margin from accordion

--- src/components/TaskList/components/TaskItem/index.js
+++ src/components/TaskList/components/TaskItem/index.js
@@ -4,12 +4,13 @@ import { connect } from "react-redux";
 import { OverlayTrigger, Popover, Button } from "react-bootstrap";
 import { updateTaskItemAction, deleteTaskItemAction } from "actions/task";
 import "./index.scss";
+import "./accordion-override.css";
 
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionActions";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme, StylesProvider } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
 
 import LinearProgress from "@material-ui/core/LinearProgress";
@@ -200,116 +201,119 @@ class TaskItem extends React.Component {
     }
 
     return (
-      <Accordion className="bg-light">
-        <AccordionSummary
-          // expandIcon={<ExpandMoreIcon />}
-          aria-label="Expand"
-          aria-controls="additional-actions1-content"
-          id="additional-actions1-header"
-          color="primary"
-        >
-          <div>
-            <form
-              className="task row mt-2 p-2 rounded align-items-center bg-light"
-              onSubmit={this.updateTaskDescription}
-            >
-              <div className="col-1 d-flex justify-content-left">
-                <button
-                  className="cursor-pointer"
-                  onClick={this.toggleCompletion}
-                  type="button"
-                  onMouseEnter={() => this.setState({ isDoneHover: true })}
-                  onMouseLeave={() => this.setState({ isDoneHover: false })}
-                >
-                  {isDoneIconState}
-                </button>
-              </div>
-              <input
-                className="title form-control shadow-none bg-light col-4 d-flex justify-content-left"
-                type="text"
-                ref={(input) => {
-                  this.descriptionInput = input;
-                }}
-                value={descInputValue}
-                onChange={this.handleDescInputChange}
-                onBlur={this.updateTaskDescription}
-                disabled={!isEditMode}
-              />
-              <div className="col-1 d-flex border-left justify-content-center">
-                <div className="align-middle">
-                  <img
-                    src="https://i.imgur.com/tToSF7j.png"
-                    width="25px"
-                    height="25px"
-                    alt="clap count"
+      <StylesProvider injectFirst>
+        <Accordion className="bg-light">
+          <AccordionSummary
+            // expandIcon={<ExpandMoreIcon />}
+            aria-label="Expand"
+            aria-controls="additional-actions1-content"
+            id="additional-actions1-header"
+            color="primary"
+            classes={{ content: "altered-content" }}
+          >
+            <div>
+              <form
+                className="task row mt-2 p-2 rounded align-items-center bg-light"
+                onSubmit={this.updateTaskDescription}
+              >
+                <div className="col-1 d-flex justify-content-left">
+                  <button
+                    className="cursor-pointer"
+                    onClick={this.toggleCompletion}
+                    type="button"
+                    onMouseEnter={() => this.setState({ isDoneHover: true })}
+                    onMouseLeave={() => this.setState({ isDoneHover: false })}
+                  >
+                    {isDoneIconState}
+                  </button>
+                </div>
+                <input
+                  className="title form-control shadow-none bg-light col-4 d-flex justify-content-left"
+                  type="text"
+                  ref={(input) => {
+                    this.descriptionInput = input;
+                  }}
+                  value={descInputValue}
+                  onChange={this.handleDescInputChange}
+                  onBlur={this.updateTaskDescription}
+                  disabled={!isEditMode}
+                />
+                <div className="col-1 d-flex border-left justify-content-center">
+                  <div className="align-middle">
+                    <img
+                      src="https://i.imgur.com/tToSF7j.png"
+                      width="25px"
+                      height="25px"
+                      alt="clap count"
+                    />
+                  </div>
+                  <div className="givenClaps">
+                    {givenClaps ? givenClaps.length : 0}
+                  </div>{" "}
+                </div>
+                <div className="col-1 d-flex border-left justify-content-center">
+                  <img className="w-25" src="/potato_mango.png" alt="mango" />
+                  <div className="mangosDonated">
+                    {this.countMangoDonations()}
+                  </div>
+                </div>
+                <div className="col-2 d-flex border-left justify-content-center">
+                  <Calendar
+                    className="cursor-pointer calendar"
+                    dueDate={dueDate}
+                    handleDateChange={this.updateDueDate}
                   />
                 </div>
-                <div className="givenClaps">
-                  {givenClaps ? givenClaps.length : 0}
-                </div>{" "}
-              </div>
-              <div className="col-1 d-flex border-left justify-content-center">
-                <img className="w-25" src="/potato_mango.png" alt="mango" />
-                <div className="mangosDonated">
-                  {this.countMangoDonations()}
-                </div>
-              </div>
-              <div className="col-2 d-flex border-left justify-content-center">
-                <Calendar
-                  className="cursor-pointer calendar"
-                  dueDate={dueDate}
-                  handleDateChange={this.updateDueDate}
-                />
-              </div>
-              <div className="col-1 d-flex border-left justify-content-center">
-                <button
-                  className="cursor-pointer"
-                  onClick={this.togglePrivacy}
-                  type="button"
-                  onMouseEnter={() => {
-                    this.setState({ isPublicHover: true });
-                  }}
-                  onMouseLeave={() => {
-                    this.setState({ isPublicHover: false });
-                  }}
-                >
-                  {isPublicIconState}
-                </button>
-              </div>
-              <div className="col-1 d-flex border-left justify-content-center">
-                <OverlayTrigger
-                  trigger="focus"
-                  placement="right"
-                  overlay={popoverRight}
-                >
-                  <a
-                    href="#editMode"
-                    tabIndex={0}
-                    className="btn btn-sm btn-light"
-                    role="button"
+                <div className="col-1 d-flex border-left justify-content-center">
+                  <button
+                    className="cursor-pointer"
+                    onClick={this.togglePrivacy}
                     type="button"
+                    onMouseEnter={() => {
+                      this.setState({ isPublicHover: true });
+                    }}
+                    onMouseLeave={() => {
+                      this.setState({ isPublicHover: false });
+                    }}
                   >
-                    <i className="material-icons">more_vert</i>
-                  </a>
-                </OverlayTrigger>
-              </div>
-              <div className="col-1 d-flex border-left justify-content-center">
-                <ExpandMoreIcon />
-              </div>
-            </form>
-            <ThemeProvider theme={theme}>
-              <LinearProgress
-                variant="determinate"
-                style={{ height: "10px" }}
-                value={this.getProgressPercentage(task)}
-              />
-            </ThemeProvider>
-          </div>
-        </AccordionSummary>
-        <AccordionDetails className="bg-dark">
-          <SubTasks task={task} />
-        </AccordionDetails>
-      </Accordion>
+                    {isPublicIconState}
+                  </button>
+                </div>
+                <div className="col-1 d-flex border-left justify-content-center">
+                  <OverlayTrigger
+                    trigger="focus"
+                    placement="right"
+                    overlay={popoverRight}
+                  >
+                    <a
+                      href="#editMode"
+                      tabIndex={0}
+                      className="btn btn-sm btn-light"
+                      role="button"
+                      type="button"
+                    >
+                      <i className="material-icons">more_vert</i>
+                    </a>
+                  </OverlayTrigger>
+                </div>
+                <div className="col-1 d-flex border-left justify-content-center">
+                  <ExpandMoreIcon />
+                </div>
+              </form>
+              <ThemeProvider theme={theme}>
+                <LinearProgress
+                  variant="determinate"
+                  style={{ height: "10px" }}
+                  value={this.getProgressPercentage(task)}
+                />
+              </ThemeProvider>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails className="bg-dark">
+            <SubTasks task={task} />
+          </AccordionDetails>
+        </Accordion>
+      </StylesProvider>
     );
   }
 }
