167fb9c38ff4b35a08967d26c4741dcb80e0bc29
AamirL1011 <aamirs@students.cs.ubc.ca>
2020-08-01 16:51:23 -0700

    Added CloudFront CDN links to deliver static assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,3 @@
 // Links to assets on AWS
-export const CLAP_IMG_URL =
-  "https://dogether-assets.s3.ca-central-1.amazonaws.com/clap.png";
-export const LOGO_URL =
-  "https://dogether-assets.s3.ca-central-1.amazonaws.com/potato_mango.png";
+export const CLAP_IMG_URL = "https://dxyhkeibwyh4n.cloudfront.net/clap.png";
+export const LOGO_URL = "https://dxyhkeibwyh4n.cloudfront.net/potato_mango.png";
