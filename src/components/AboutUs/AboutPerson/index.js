87075e27d555acce53364b265090830c62c69294
Ryan Oh <ohryan55@gmail.com>
2020-08-01 23:41:54 -0700

    added about us responsive for mobile

--- src/components/AboutUs/AboutPerson/index.js
+++ src/components/AboutUs/AboutPerson/index.js
@@ -6,26 +6,42 @@ const AboutPerson = (props) => {
   const { avatar, name, desc, linkedInUrl } = props;
 
   return (
-    <div className="col-3 justify-content-center align-content-center">
-      <div className="row justify-content-center about-person-entry">
-        <img
-          src={avatar}
-          width="60px"
-          height="60px"
-          className="user-avatar"
-          alt=""
-        />
-      </div>
-      <div className="row justify-content-center about-person-entry">
-        {name}
-      </div>
-      <div className="row justify-content-center about-person-entry">
-        {desc}
-      </div>
-      <div className="row justify-content-center about-person-entry">
-        <Button onClick={() => window.open(linkedInUrl, "_blank")}>
-          More Info
-        </Button>
+    <div className="col-lg-3 col-sm-12 justify-content-center align-content-center">
+      <div className="row">
+        <div className="col-3 col-sm-3 col-lg-12">
+          <img
+            src={avatar}
+            width="60px"
+            height="60px"
+            className="user-avatar"
+            alt=""
+          />
+          {/* <div className="row justify-content-center about-person-entry"> */}
+          {/*  */}
+          {/* </div> */}
+        </div>
+        <div className="col-3 col-sm-3 col-lg-12">
+          {name}
+          {/* <div className="row justify-content-center about-person-entry"> */}
+          {/*  */}
+          {/* </div> */}
+        </div>
+        <div className="col-3 col-sm-3 col-lg-12">
+          {desc}
+          {/* <div className="row justify-content-center about-person-entry"> */}
+          {/*  {desc} */}
+          {/* </div> */}
+        </div>
+        <div className="col-3 col-sm-3 col-lg-12">
+          <Button onClick={() => window.open(linkedInUrl, "_blank")}>
+            More Info
+          </Button>
+          {/* <div className="row justify-content-center about-person-entry"> */}
+          {/*  <Button onClick={() => window.open(linkedInUrl, "_blank")}> */}
+          {/*    More Info */}
+          {/*  </Button> */}
+          {/* </div> */}
+        </div>
       </div>
     </div>
   );
