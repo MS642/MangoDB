b050801da1c44c5d5b126db877a34a4c978e0ac1
MS642 <mabouzaid642@gmail.com>
2020-08-03 01:58:59 -0700

    Profile task feed

--- src/components/UserProfile/components/ProfileFeed/index.js
+++ src/components/UserProfile/components/ProfileFeed/index.js
@@ -1,9 +1,97 @@
 import * as React from "react";
+import { connect } from "react-redux";
+import { sumMangos } from "services/mangoTransactions";
+import { fetchUserCompletedTasks } from "actions/feedActions";
+
+import CompletedTask from "components/Feed/components/TaskUnit/components/CompletedTask/index";
+import SocialUnit from "components/Feed/components/TaskUnit/components/SocialUnit/index";
 
 class ProfileFeed extends React.Component {
+  componentDidMount() {
+    const { user, fetchUserCompletedTasks: fetchTasks } = this.props;
+    fetchTasks(user);
+  }
+
   render() {
-    return <div>PUBLIC TASKS SHOWN HERE</div>;
+    const { user, tasksUser, isCurrUser } = this.props;
+    // fetchTasks(user);
+    let taskFeedList = null;
+    if (tasksUser) {
+      taskFeedList = tasksUser.map((taskF) => {
+        const {
+          _id,
+          description,
+          timestamp,
+          givenClaps,
+          mangoTransactions,
+          user_id,
+          isDone,
+          completionTimestamp,
+          startTimestamp,
+        } = taskF;
+        const { avatar, username, badges, profileUrl } = user;
+        return (
+          <div key={_id} className="feedPad">
+            <div className="row justify-content-center TaskUnit bg-light text-dark">
+              <div className="col TaskCol">
+                <div>
+                  <CompletedTask
+                    avatar={avatar}
+                    name={username}
+                    taskMessage={description}
+                    startTimestamp={startTimestamp}
+                    isDone={isDone}
+                    completionTimestamp={completionTimestamp}
+                    lastUpdated={timestamp}
+                    badges={badges}
+                    profileUrl={profileUrl}
+                    isCurrUser={isCurrUser}
+                  />
+                  <SocialUnit
+                    taskID={_id}
+                    taskUserID={user_id}
+                    name={username}
+                    clapNum={givenClaps.length}
+                    givenClap={givenClaps}
+                    mangoNum={sumMangos(mangoTransactions)}
+                    isDone={isDone}
+                    currUser={user._id}
+                    profileTasks
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      });
+    }
+    return (
+      <div>
+        {isCurrUser ? (
+          <h1 style={{ textAlign: "center" }}>
+            Your most recent completed tasks
+          </h1>
+        ) : (
+          <h1 style={{ textAlign: "center" }}>
+            {user.username}&apos;s most recent completed tasks
+          </h1>
+        )}
+        <br />
+        <div className="taskListFeed container justify-content-center align-self-center">
+          {taskFeedList}
+        </div>
+      </div>
+    );
   }
 }
 
-export default ProfileFeed;
+// state has entire state of app!!
+const mapStateToProps = (state) => {
+  return {
+    tasksUser: state.feedDB.tasksUser,
+  };
+};
+
+export default connect(mapStateToProps, { fetchUserCompletedTasks })(
+  ProfileFeed
+);
