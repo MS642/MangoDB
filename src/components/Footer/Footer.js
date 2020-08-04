d8c0a2909aa0b7ec872694eaf448fced179dea23
Ryan Oh <ohryan55@gmail.com>
2020-08-03 23:39:32 -0700

    home-page resizing text. and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 
+import "./footer.css";
+
 class Footer extends React.Component {
   render() {
     return (
-      <div className="bg-dark text-light">
+      <div className="footer-text bg-dark text-light">
         <hr />
         Copyright @ MangoDB 2020. All rights reserved.
       </div>
