478aa6ec9c8d4ba2dc1ce56909f11d005586bfb1
maxahn <ahnmaximus@gmail.com>
2020-08-05 15:53:55 -0700

    fixed task title sizing

--- src/scenes/Pages/TaskPage/index.js
+++ src/scenes/Pages/TaskPage/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import TaskView from "components/TaskView";
+import "./index.scss";
 
 class TaskPage extends React.Component {
   render() {
@@ -9,7 +10,9 @@ class TaskPage extends React.Component {
           <br />
           <div className="row">
             <div className="col-12 d-flex justify-content-center align-items-center">
-              <h1 className="display-3">What You&apos;re Up To</h1>
+              <h1 className="display-3 taskPageTitle">
+                What You&apos;re Up To
+              </h1>
             </div>
           </div>
           <TaskView />
